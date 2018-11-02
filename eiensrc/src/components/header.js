9a82d4545227c2a0303655ee50440e3497370600
ruchir88 <ruchir88@gmail.com>
2018-11-01 23:24:35 -0400

    initial commit with header and app component

---
+++ eiensrc/src/components/header.js
@@ -0,0 +1,38 @@
+// import libraries for making the component
+
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+// create component
+const Header = (props) => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text style={styles.headerTextStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    viewStyle : {
+        backgroundColor: 'rgba(30,144,255,1.0)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 90,
+        shadowColor: 'rgba(30,144,255,1.0)',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    headerTextStyle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white',
+        marginTop: 30
+    }
+});
+
+// Make the component available to other parts of the app
+export default Header;
+
+
